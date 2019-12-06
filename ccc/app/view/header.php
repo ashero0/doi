3d12838d8d65bf6fda9511838a2ed92695b081e0
Heather Robinson <hnr2800@vt.edu>
2019-12-06 15:16:58 -0500

    styling for bootstrap (maroon, fonts, etc.) and start on headers

--- ccc/app/view/header.php
+++ ccc/app/view/header.php
@@ -27,6 +27,7 @@
 
 	<!-- Import Bootstrap stuff -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?= BASE_URL ?>/public/css/custom.css">
 </head>
 
 
@@ -35,6 +36,17 @@
 
 <body>
 
+	<div id="universal-access">
+		<nav class="navbar navbar-dark bg-dark">
+			<div class="container-fluid">
+				<ul class="nav navbar-nav navbar-right">
+					<!-- <li><img src="<?= BASE_URL ?>/public/assets/universalaccess.svg"></li> -->
+					<!-- <li>test</li> -->
+				</ul>
+			</div>
+		</nav>
+	</div>
+
 	<!-- Header -->
 	<div id="header">
 		<nav class="navbar navbar-light bg-light">
@@ -42,6 +54,52 @@
 				Cook Counseling Center
 			</a>
 		</nav>
+	</div>
+
+	<div id="navigation">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			<ul class="navbar-nav">
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			  		About
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+			  		<a class="dropdown-item" href="#">About</a>
+			  		<a class="dropdown-item" href="#">Staff</a>
+			  		<a class="dropdown-item" href="#">Hours & Location</a>
+				</div>
+		  	</li>
+		  </ul>
+		</nav>
+		<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<a class="navbar-brand" href="#">Navbar</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+			<ul class="navbar-nav">
+			  <li class="nav-item active">
+				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="#">Features</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="#">Pricing</a>
+			  </li>
+			  <li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				  Dropdown link
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+				  <a class="dropdown-item" href="#">Action</a>
+				  <a class="dropdown-item" href="#">Another action</a>
+				  <a class="dropdown-item" href="#">Something else here</a>
+				</div>
+			  </li>
+			</ul>
+		  </div>
+		</nav> -->
 	</div>
 
 	<div class="page-content">
