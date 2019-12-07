0a9c7c3483acc5666112adf8a0ca7b13ad688acb
Barbara O'Connor <barbaraoconnor@Barbaras-MacBook-Pro.local>
2019-12-07 14:22:25 -0500

    footer is almost done just needs some tweaking

--- ccc/app/view/footer.php
+++ ccc/app/view/footer.php
@@ -1,4 +1,5 @@
 <footer>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </div>
 	
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -6,6 +7,51 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 <body>
-	
+	<div class="row">
+	<div class="col-md-2" style="background-color: #000000;">
+	<p>
+	<br>
+	<img width=225 height=99 class="vt-map-image" src="http://www.assets.cms.vt.edu/images/vt-campuses.svg" longdesc="https://vt.edu/maps.html#locations" alt="Map of Virginia with pins showing locations of Virginia Tech campuses. See All Locations link which follows provides details">
+	</p>	
+	</div>
+	<div class="col-md-4" style="background-color: #000000;">
+	<p>
+	<br>
+	<img width=160 height=32 class="vt-footer-logo" src="http://www.assets.cms.vt.edu/images/logo-white-black.svg" alt="Virginia Tech logo">	
+	<a href="#" style="color: #FFFFFF"><h6><i>GET DIRECTIONS&nbsp;&nbsp;<i class="fa fa-arrow-right" style="color: #FFFFFF"></i></i></h6></a>
+	<a href="#" style="color: #FFFFFF"><h6><i>SEE ALL LOCATIONS&nbsp;&nbsp;<i class="fa fa-arrow-right" style="color: #FFFFFF"></i></i></h6></a>
+	</p>
+	</div>
+	<div class="col-md-2" style="background-color: #000000;">
+	<p>
+	<br>
+	<a href="#" style="color: #FFFFFF">University Status</a><br>
+	<a href="#" style="color: #FFFFFF">Acceptable Use</a><br>
+	<a href="#" style="color: #FFFFFF">Accessibility</a><br>
+	<a href="#" style="color: #FFFFFF">Policies</a><br>
+	<a href="#" style="color: #FFFFFF">University Bookstore</a><br>
+	</p>
+	</div>
+	<div class="col-md-2" style="background-color: #000000;">
+	<p>
+	<br>
+	<a href="#" style="color: #FFFFFF">Contact Virginia Tech</a><br>
+	<img width="20" height="20" src="https://www.ucc.vt.edu/global_assets/images/vt-2016-ribbon.svg"><a href="https://www.weremember.vt.edu/" style="color: #FFFFFF">We Remember</a><br>
+	<a href="#" style="color: #FFFFFF">Consumer Information</a><br>
+	<a href="#" style="color: #FFFFFF">Equal Opportunity</a><br>
+	<a href="#" style="color: #FFFFFF">Jobs at Virginia Tech</a><br>
+	</p>
+	</div>
+	<div class="col-md-2" style="background-color: #000000;">
+	<p>
+    <br>
+	<a href="#" style="color: #FFFFFF">Privacy Statement</a><br>
+	<a href="#" style="color: #FFFFFF">University Libraries</a><br>
+	<a href="#" style="color: #FFFFFF">Stop Abuse</a><br>
+	<a href="#" style="color: #FFFFFF">WVTF</a><br>
+	<a href="#" style="color: #FFFFFF">Strategic Plan</a><br>
+	</p>
+	</div>
+</div>
 </body>
 </footer>
