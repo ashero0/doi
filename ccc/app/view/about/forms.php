7c324e939f6d2cfdb1567c7e67a8f555bd157d70
Hithesh Peddamekala <hitp98@vt.edu>
2019-12-07 15:47:12 -0500

    add contact to forms pg

--- ccc/app/view/about/forms.php
+++ ccc/app/view/about/forms.php
@@ -1,4 +1,22 @@
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
-<p>forms</p>
+<h1>Forms</h1>
+<h3>Release of Information</h3>
+<p>
+	<a href="https://www.ucc.vt.edu/content/dam/ucc_vt_edu/assets/docs/CCC-Release-of-Information-8-13-2018.docx" target="_blank">Authorization for Release of Information Form</a>
+</p>
+<h3>Academic Relief Forms</h3>
+<p>
+	<a href="https://www.ucc.vt.edu/content/dam/ucc_vt_edu/assets/docs/Application%20Packet%20for%20Academic%20Relief.pdf" target="_blank">Application Packet for Academic Relief</a>: This packet contains two forms. A completed application packet must include
+	<u>both</u>: 1) Academic Relief Request Form completed by the student, and 2) Dean's Academic Advising Form signed by the Associate
+	Dean of the student's academic college.
+	<b>If the applicant is an international student</b>, the Advising Form should also be signed by staff at Cranwell International
+	Center.
+	<b>If the applicant is a Graduate International student</b>, the advising form should be signed by an international advisor
+	at the Graduate School.</p>
+<p>If a student is submitting supporting information from an off-campus provider, an
+	<a href="https://www.ucc.vt.edu/content/dam/ucc_vt_edu/assets/docs/Form_For_Outside_Provider.pdf"
+	 target="_blank">Outside Provider Documentation Form to Support Academic Relief Application</a> may also be submitted with the application
+	packet. </p>
+<p>When a student is preparing to return to Virginia Tech following a medical withdrawal for mental health reasons, a
+	<a href="https://www.ucc.vt.edu/content/dam/ucc_vt_edu/assets/docs/Form_for_Reenrollment.docx"
+	 target="_blank">Provider Form for Re-enrollment</a> must be completed by their counseling or psychiatric provider and submitted to Cook
+	Counseling Center in order to authorize the removal of the registration hold on the student's account.</p>
