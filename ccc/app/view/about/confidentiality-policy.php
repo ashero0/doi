bcf4b44b9a8d5325c22ee8c34dae2acc64c810a6
Hithesh Peddamekala <hitp98@vt.edu>
2019-12-07 12:46:22 -0500

    added content confidentiality policy pg

--- ccc/app/view/about/confidentiality-policy.php
+++ ccc/app/view/about/confidentiality-policy.php
@@ -1,4 +1,21 @@
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
-<p>confidentiality policy</p>
+<div class="row justify-content-md-center">
+	<div class="col-md-9">
+		<h1>Confidentiality Policy</h1>
+	</div>
+	<div class="col-md-9">
+		<p>
+			Cook Counseling Center values the privacy of its clients and the confidentiality of the personal and health information entrusted
+			to us. In order to protect this privacy we have policies and procedures to limit disclosures of personal information to
+			those minimally necessary for the medical care of the client, those for which the client has given permission and/or those
+			required by law or public safety.
+		</p>
+		<p>
+			To release information to an outside provider or family member, a student must fill out the 
+            <a href="https://www.ucc.vt.edu/content/dam/ucc_vt_edu/assets/docs/CCC-Release-of-Information-8-13-2018.docx">Authorization Release Form</a>.
+		</p>
+		<p>
+			American Psychological Association article: 
+            <a href="https://www.apa.org/helpcenter/confidentiality" target="_blank">Protecting Your Privacy: Understanding Confidentiality</a>
+		</p>
+	</div>
+</div>
