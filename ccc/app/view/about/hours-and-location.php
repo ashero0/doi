f21a967e81e4c27031f145043ca2bdd8d186b0f7
Nicole Moore <nicolemoore@Johns-MacBook-Air.local>
2019-12-07 12:50:24 -0500

    format hours and location page

--- ccc/app/view/about/hours-and-location.php
+++ ccc/app/view/about/hours-and-location.php
@@ -1,67 +1,65 @@
 <div class="row">
-<div class="col-md-8">
-<h1>Hours and Location</h1>
-<h3><i>Hours:</i></h3>
-<p style="margin-bottom: 0px;">Monday - Thursday: 8 a.m. - 5 p.m.</p>
-<p>Friday: 9 a.m. - 5 p.m.</p>
+    <div class="col-md-8">
+        <h1>Hours and Location</h1>
+        <h3><i>Hours:</i></h3>
+        <p style="margin-bottom: 0px;">Monday - Thursday: 8 a.m. - 5 p.m.</p>
+        <p>Friday: 9 a.m. - 5 p.m.</p>
 
-</div>
-<img class="col-md-4" src="https://www.ucc.vt.edu/content/ucc_vt_edu/en/about/hours_location/_jcr_content/content/adaptiveimage.transform/m-medium/image.jpg">
+    </div>
+    <img class="col-md-4"
+        src="https://www.ucc.vt.edu/content/ucc_vt_edu/en/about/hours_location/_jcr_content/content/adaptiveimage.transform/m-medium/image.jpg">
 </div> <!-- end of row div -->
 <!--12 total. image is md-4 and 3 things in bottom are md-4 -->
 
 <div class="row">
-<div class="col-md-8">
-<h3><i>Locations:</i></h3>
-</div>
-</div>
-<div class="row">
-<div class="col-md-4">
-	<h4><i><u>Main Office:</u></i></h4>
-<p>
-Cook Counseling Center<br>
-McComas Hall, RM 240, Virginia Tech<br>
-895 Washington Street SW<br>
-Blacksburg, VA 24061<br>
-Phone: 540-231-6557<br>
-Fax: 540-231-2104
-</p>
-</div>
-<div class="col-md-4">
-	<h4><i><u>Satellite Office:</u></i></h4>
-<p>
-Cook Counseling Center at East Eggleston<br>
-East Eggleston, RM 107, Virginia Tech<br>
-500 Drillfield Drive<br>
-Blacksburg, VA 24061<br>
-Phone: 540-231-7809<br>
-Fax: 540-231-9660
-</p>
-</div>
+    <div class="col-md-8">
+        <h3><i>Locations:</i></h3>
+    </div>
 </div>
 <div class="row">
-<div class="col-md-4">
-	<h4><i><u>Satellite Office:</u></i></h4>
-<p>
-Cook Counseling Center<br>
-4505 North End Center<br>
-Virginia Tech<br>
-Blacksburg, VA 24061<br>
-Phone: 540-231-3968
-</p>
+    <div class="col-md-4">
+        <h4><i><u>Main Office:</u></i></h4>
+        <p>
+            Cook Counseling Center<br>
+            McComas Hall, RM 240, Virginia Tech<br>
+            895 Washington Street SW<br>
+            Blacksburg, VA 24061<br>
+            Phone: 540-231-6557<br>
+            Fax: 540-231-2104
+        </p>
+    </div>
+    <div class="col-md-4">
+        <h4><i><u>Satellite Office:</u></i></h4>
+        <p>
+            Cook Counseling Center at East Eggleston<br>
+            East Eggleston, RM 107, Virginia Tech<br>
+            500 Drillfield Drive<br>
+            Blacksburg, VA 24061<br>
+            Phone: 540-231-7809<br>
+            Fax: 540-231-9660
+        </p>
+    </div>
 </div>
-<div class="col-md-4">
-	<h4><i><u>Satellite Office:</u></i></h4>
-<p>
-Cook Counseling Center at Kent Square<br>
-Kent Square, Suite 201-202<br>
-202 South Main Street<br>
-Blacksburg, VA 24060<br>
-Phone: 540-231-6557<br>
-Fax: 540-231-3989
-</p>
+<div class="row">
+    <div class="col-md-4">
+        <h4><i><u>Satellite Office:</u></i></h4>
+        <p>
+            Cook Counseling Center<br>
+            4505 North End Center<br>
+            Virginia Tech<br>
+            Blacksburg, VA 24061<br>
+            Phone: 540-231-3968
+        </p>
+    </div>
+    <div class="col-md-4">
+        <h4><i><u>Satellite Office:</u></i></h4>
+        <p>
+            Cook Counseling Center at Kent Square<br>
+            Kent Square, Suite 201-202<br>
+            202 South Main Street<br>
+            Blacksburg, VA 24060<br>
+            Phone: 540-231-6557<br>
+            Fax: 540-231-3989
+        </p>
+    </div>
 </div>
-</div>	
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
--- ccc/app/view/services/emergency-services.php
+++ ccc/app/view/services/emergency-services.php
@@ -1,4 +1,5 @@
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
-<p>emergency services</p>
+<h1>Emergency Services</h1>
+<h3><i>Worried about Self-Harm or Suicidality?</i></h3>
+<p>If you are concerned about yourself or any Virginia Tech student being (or becoming) suicidal,
+    please call the Cook Counseling Center at 540-231-6557 or one of the other resources listed below. The Cook
+    Counseling Center number can be used during regular office hours or after-hours to speak with a counselor.</p>
