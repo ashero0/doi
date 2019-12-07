92818d3906028e70a44aee2623d8f7f857482c43
Hithesh Peddamekala <hitp98@vt.edu>
2019-12-07 13:03:11 -0500

    added content to about-cook pg

--- ccc/app/view/about/about-cook.php
+++ ccc/app/view/about/about-cook.php
@@ -1,4 +1,41 @@
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
-<p>about cook counseling center</p>
+<div class="row justify-content-md-center">
+	<div class="col-md-9">
+		<h1>About Cook Counseling Center</h1>
+	</div>
+	<div class="col-md-9">
+		<p>
+			Thomas E. Cook Counseling Center provides opportunities for students to learn more about themselves as individuals, form
+			deeper relationships with their peers, and grow to benefit our community and society at large. Cook Counseling Center
+			staff provides these learning opportunities through individual and group counseling, psychiatric care and psychotherapy,
+			and educational programming.
+		</p>
+		<p>
+			Similarly, the staff also provides support to the Virginia Tech community through cooperative programming and consultation.
+			They are guided by the highest professional and ethical standards of their professions and are dedicated to providing
+			quality services to the student population.
+		</p>
+		<p>
+			Cook Counseling Center supports the academic mission of the university through the enhancement of student learning and retention.
+			Cook Counseling Center is committed to the development of the mental healthcare profession while providing training opportunities
+			to graduate students, predoctoral interns and postdoctoral residents.
+		</p>
+		<h3> Multicultural Statement </h3>
+		<p>
+			Cook Counseling Center staff is guided in its professional activities by the ethics of their professions and by the Virginia
+			Tech Principles of Community. Cook Counseling Center is committed to delivering professional services that respect and
+			value our humanity, including differences and similarities of age, gender, gender identity, gender expression, race, ethnicity,
+			culture, national origin, religion, sexual orientation, disability, language, political affiliation, socioeconomic status,
+			genetic information, or veteran status.
+		</p>
+		<p>
+			Prejudice and discrimination are inimical to the practice of counseling, where the value of each human being is prized. Cook
+			Counseling Center’s professional guidelines call upon its staff to recognize prejudice and discrimination in all of its
+			forms, to become aware of their effects on individuals and the community and to overcome the limitations and restrictions
+			they place on each of us through learning, support and teaching.
+		</p>
+		<p>
+			In promotion of a just and caring university, the Cook Counseling Center staff will seek to promote social justice in all
+			of its activities, including counseling, supervision, education, training, outreach and hiring practices.
+		</p>
+	</div>
+</div>
