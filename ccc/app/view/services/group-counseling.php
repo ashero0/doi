201490c5fc7ca9019bb41cc453bd22c9dc9bdb46
Nicole Moore <nicolemoore@Johns-MacBook-Air.local>
2019-12-07 15:06:04 -0500

    group counseling page

--- ccc/app/view/services/group-counseling.php
+++ ccc/app/view/services/group-counseling.php
@@ -1,4 +1,253 @@
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
-<p>group counseling</p>
+<h1>Group Counseling</h1>
+
+<p>Group counseling is one of our primary clinical services. For many students, group therapy is the most
+    effective and appropriate treatment option.
+
+    Group counseling is a unique opportunity available to students in need of a supportive environment. It
+    allows students to hear multiple perspectives and receive encouragement and feedback from peers in a safe
+    and confidential environment. These interpersonal interactions deepen self-awareness and help members learn
+    new ways of relating to others. While some of our groups focus primarily on deepening this understanding of
+    self and others, we also have groups that focus on specific concerns and skill development.
+
+    There is no limit to the number of group therapy sessions available to students. Getting started with group
+    counseling begins with an initial evaluation, or intake appointment. You and your counselor will discuss if
+    group counseling is the appropriate form of treatment. Your counselor will also provide information about
+    our groups so you can choose the one that best suits your needs. Groups meet weekly for 60-90 minutes. There
+    are typically 5-12 students in each group and 1-2 group leaders.
+
+    Check out our videos and the links below for more information about our group program. </p>
+
+<div id="accordion">
+    <div class="card">
+        <div class="card-header" id="headingOne">
+            <h5 class="mb-0">
+                <button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false"
+                    aria-controls="collapseOne">
+                    Understanding Self and Others
+                </button>
+            </h5>
+        </div>
+        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="card-body">
+                This group is for students who wish to increase self-understanding, self-esteem, and intimacy in
+                relationships, as well as addressing their interpersonal style and relationship-building skills. This is
+                an opportunity to receive support and feedback from other students in a safe environment while
+                developing your ability to understand and express yourself. Both undergraduate and graduate groups are
+                available
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header" id="headingTwo">
+            <h5 class="mb-0">
+                <button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false"
+                    aria-controls="collapseTwo">
+                    Substance Use Group
+                </button>
+            </h5>
+        </div>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+            <div class="card-body">
+                This group is for students who have concerns about their substance use behaviors. Students are
+                encouraged to set personal goals for their use based on the stages of change model.
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header" id="headingThree">
+            <h5 class="mb-0">
+                <button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false"
+                    aria-controls="collapseThree">
+                    DBT Skills Group
+                </button>
+            </h5>
+        </div>
+        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+            <div class="card-body">
+                This group is a psychoeducation group based upon Dialectical Behavioral Therapy. It teaches skills in
+                Mindfulness, Distress Tolerance, Emotion Regulation, and Interpersonal Effectiveness.
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header" id="headingFour">
+            <h5 class="mb-0">
+                <button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false"
+                    aria-controls="collapseFour">
+                    DBT Skills Group for Eating Disorders
+                </button>
+            </h5>
+        </div>
+        <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+            <div class="card-body">
+                This group is structured to help those who struggle with eating issues learn effective coping skills in
+                order to reduce suffering and improve self-acceptance and general life satisfaction.
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header" id="headingFive">
+            <h5 class="mb-0">
+                <button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false"
+                    aria-controls="collapseFive">
+                    Working Through Trauma
+                </button>
+            </h5>
+        </div>
+        <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+            <div class="card-body">
+                This support group is being offered by the Cook Counseling Center and the Women's Center. This group is
+                for female-identified survivors of sexual trauma or violence who may benefit from peer support and
+                education.
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header" id="headingSix">
+            <h5 class="mb-0">
+                <button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false"
+                    aria-controls="collapseSix">
+                    The Moose Group
+                </button>
+            </h5>
+        </div>
+        <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+            <div class="card-body">
+                This group is for students who have experienced severe mental illness and/or hospitalization. It is open
+                to those who are interested in receiving support, feedback, and increasing their understanding of how
+                their mental illness affects their lives and their relationships.
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header" id="headingSeven">
+            <h5 class="mb-0">
+                <button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false"
+                    aria-controls="collapseSeven">
+                    Mindfulness for Anxiety
+                </button>
+            </h5>
+        </div>
+        <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
+            <div class="card-body">
+                This group teaches mindfulness practices that help manage anxiety. Each week, members practice
+                mindfulness exercises and discuss how they are moving through their anxiety to pursue the things that
+                matter to them.
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header" id="headingEight">
+            <h5 class="mb-0">
+                <button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false"
+                    aria-controls="collapseEight">
+                    Grief Group
+                </button>
+            </h5>
+        </div>
+        <div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordion">
+            <div class="card-body">
+                This group is for individuals who have experienced the death / imminent death of a loved one. It
+                provides a combination of psychoeducation and discussion in a safe, nonjudgmental space with others who
+                are there to listen.
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header" id="headingNine">
+            <h5 class="mb-0">
+                <button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false"
+                    aria-controls="collapseNine">
+                    Body Movement Group
+                </button>
+            </h5>
+        </div>
+        <div id="collapseNine" class="collapse" aria-labelledby="headingNine" data-parent="#accordion">
+            <div class="card-body">
+                This is a group guided physical yoga and meditation practive to use as tools for anxiety management. It
+                incorporates supportive conversation to encourage self-reflection and connection. Individuals are
+                required to participate in concurrent individual therapy.
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header" id="headingTen">
+            <h5 class="mb-0">
+                <button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseTen" aria-expanded="false"
+                    aria-controls="collapseTen">
+                    Social Skills Gaming Group
+                </button>
+            </h5>
+        </div>
+        <div id="collapseTen" class="collapse" aria-labelledby="headingTen" data-parent="#accordion">
+            <div class="card-body">
+                This group uses a fantasy role-playing game to help individuals with Autism Spectrum Disorder develop
+                social skills. Including perspective-taking, tolerating frustration, problem-solving, communication and
+                collaboration.
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header" id="headingEleven">
+            <h5 class="mb-0">
+                <button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseEleven" aria-expanded="false"
+                    aria-controls="collapseEleven">
+                    Expressive Arts with Wagner
+                </button>
+            </h5>
+        </div>
+        <div id="collapseEleven" class="collapse" aria-labelledby="headingEleven" data-parent="#accordion">
+            <div class="card-body">
+                This class engages members in different art modalities to promote stress management and personal growth.
+                This group facilitates creativity, self-reflection and connection. No previous artistic experience is
+                necessary.
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header" id="headingTwelve">
+            <h5 class="mb-0">
+                <button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseTwelve" aria-expanded="false"
+                    aria-controls="collapseTwelve">
+                    Our Voice
+                </button>
+            </h5>
+        </div>
+        <div id="collapseTwelve" class="collapse" aria-labelledby="headingTwelve" data-parent="#accordion">
+            <div class="card-body">
+                A support group, held outside of Cook Counseling, for any student of a marginalized racial or ethnic
+                background to glean space to explore shared difficulties pertaining to their identities.
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header" id="headingThirteen">
+            <h5 class="mb-0">
+                <button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseThirteen"
+                    aria-expanded="false" aria-controls="collapseThirteen">
+                    Facing Our Fears
+                </button>
+            </h5>
+        </div>
+        <div id="collapseThirteen" class="collapse" aria-labelledby="headingThirteen" data-parent="#accordion">
+            <div class="card-body">
+                This focuses on systematically confronting worrying thoughts in order to decrease obsessive thought or
+                compulsive thoughts / behaviors. This group is for anyone who has repetitive worries or behaviors that
+                get in the way of their daily functioning.
+            </div>
+        </div>
+    </div>
+
+</div>
