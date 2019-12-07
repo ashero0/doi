a7f0b3188d56f19fd97e2868de06a215a9df6b40
Hithesh Peddamekala <hitp98@vt.edu>
2019-12-07 16:16:48 -0500

    started outreach and graduate training pgs

--- ccc/app/view/about/outreach.php
+++ ccc/app/view/about/outreach.php
@@ -1,4 +1,18 @@
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
-<p>outreach</p>
+<h1>Outreach</h1>
+<p>Outreach and consultation services are an integral part of Cook Counseling Center’s mission of reaching out to students,
+	faculty and staff through the provision of educational programming and workshops.</p>
+<p>These programs and workshops are designed to enhance the learning and development of students, faculty and staff. Outreach
+	services also include providing consultation when there is a concern about the behavior or well-being of a student. Programs
+	that have been requested in the past include issues such as eating disorders, stress management, time management, healthy
+	relationships, depression in college students, and orientation to Cook Counseling Center’s services.</p>
+<p>Our staff members also work with faculty members who would like presentations or facilitation of discussions that address
+	specific concerns of college students and complement their course content. The staff works with faulty members to develop
+	programs specific to their needs while also offering programs for special, national events, such as National Depression
+	Screening Day and Eating Disorder Awareness week.</p>
+
+<p>
+	<i>To secure one of the above outreach programs, please fill out the
+		<a href="https://virginiatech.qualtrics.com/jfe/form/SV_0rJh8d0hHCT7OBv"
+		 target="_blank">Outreach Request Form.</a>
+	</i>
+</p>
