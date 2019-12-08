bc79c25886f5e896a229e62fd1487eeeb75aed1b
Hithesh Peddamekala <hitp98@vt.edu>
2019-12-08 15:44:27 -0500

    finished outreach pg

--- ccc/app/view/about/outreach.php
+++ ccc/app/view/about/outreach.php
@@ -10,9 +10,47 @@
 	programs specific to their needs while also offering programs for special, national events, such as National Depression
 	Screening Day and Eating Disorder Awareness week.</p>
 
+<div id="accordion">
+	<div class="card">
+		<div class="card-header" id="headingOne">
+			<h5 class="mb-0">
+				<button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+					Available Programs
+				</button>
+			</h5>
+		</div>
+		<div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+			<div class="card-body">
+				<ul>
+					<li>Stress Management</li>
+					<li>How to Refer a Distressed Student</li>
+					<li>Trauma Response (after a loss)</li>
+					<li>Adjusting to Freshman Year</li>
+					<li>QPR Gatekeeper Training (Suicide Prevention)</li>
+					<li>Orientation to Cook Counseling Center’s Services</li>
+					<li>How to Refer a Friend to Cook Counseling Center</li>
+					<li>Healthy Relationships</li>
+					<li>Body Image Issues</li>
+					<li>Eating Disorders</li>
+					<li>Referring International Students</li>
+					<li>How to Identify Students in Distress</li>
+					<li>Understanding Diversity</li>
+					<li>Depression in African Americans</li>
+					<li>Public Speaking</li>
+					<li>Communication and Listening Skills</li>
+					<li>Getting Along with Your Roommate</li>
+					<li>Exercise and Mental Health</li>
+					<li>Culture Shock: How to Manage it and what to Expect</li>
+					<li>Managing Emotions in a Healthy Way</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+<br>
+
 <p>
 	<i>To secure one of the above outreach programs, please fill out the
-		<a href="https://virginiatech.qualtrics.com/jfe/form/SV_0rJh8d0hHCT7OBv"
-		 target="_blank">Outreach Request Form.</a>
+		<a href="https://virginiatech.qualtrics.com/jfe/form/SV_0rJh8d0hHCT7OBv" target="_blank">Outreach Request Form.</a>
 	</i>
 </p>
