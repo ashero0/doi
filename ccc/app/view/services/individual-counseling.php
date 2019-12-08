38dde1ae18f3b5b76c8b0231bdec43e9b9739e76
Nicole Moore <nicolemoore@Johns-MacBook-Air.local>
2019-12-08 15:51:56 -0500

    individual counseling page - not done

--- ccc/app/view/services/individual-counseling.php
+++ ccc/app/view/services/individual-counseling.php
@@ -1,4 +1,29 @@
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
-<p>individual counseling</p>
+<h1>Individual Counseling</h1>
+
+After the initial assessment appointment, you and your counselor may agree that a course of individual therapy is the
+most appropriate treatment for you. You and the therapist will identify an area of concern for your work and establish
+goals for therapy. On average, students are seen for 4-5 sessions, meeting every 2-3 weeks. In the majority of cases,
+this will meet your needs. If you would like to do additional work, you may be referred to the group therapy program.
+For problems requiring more extensive work, a referral to a community provider is recommended.
+
+<br>
+<br>
+<h3>Student Difficulties Might Include</h3>
+<ul>
+    <li>Anxiety</li>
+    <li>Depression</li>
+    <li>Eating issues</li>
+    <li>Loneliness</li>
+    <li>Feelings of inadequacy</li>
+    <li>Problems studying</li>
+    <li>Problems relating to others</li>
+    <li>Sexual concerns</li>
+    <li>Excessive use of alcohol and/or drugs</li>
+    <li>Couples issues</li>
+</ul>
+
+<img srcset="https://www.ucc.vt.edu/content/ucc_vt_edu/en/clinical_services_students/individual_counseling/jcr:content/content/adaptiveimage.transform/l-medium/image.jpg"
+    sizes=" (min-width: 992px) calc(.90 * 60vw), 90vw"
+    src="/content/ucc_vt_edu/en/clinical_services_students/individual_counseling/_jcr_content/content/adaptiveimage.transform/m-medium/image.jpg"
+    alt="counseling11">
+	</img>
