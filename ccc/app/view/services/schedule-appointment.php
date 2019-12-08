72c32c36b60d60a0ac395ad1d759c1b5982805f8
Hithesh Peddamekala <hitp98@vt.edu>
2019-12-07 19:51:56 -0500

    started input button for schedule apt

--- ccc/app/view/services/schedule-appointment.php
+++ ccc/app/view/services/schedule-appointment.php
@@ -1,4 +1,16 @@
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
-<p>schedule an appointment</p>
+<h1>Make An Appointment</h1>
+<div class="input-group mb-3" id="IDInput">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="inputGroup-sizing-default">Enter Student ID:</span>
+  </div>
+  
+  <input type="number" min="0" max="5" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+  <div class="input-group-append">
+    <button class="btn btn-outline-primary" type="button">Submit</button>
+  </div>
+</div>
+
+<!--
+<div class="input group mb-3">
+	<input type="number" min="0" max="999999999" required>
+</div> -->
