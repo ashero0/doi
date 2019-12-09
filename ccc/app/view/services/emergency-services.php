138a3bc9d10b39839a3101d5a75b6125c15599b6
Nicole Moore <nicolemoore@Johns-MacBook-Air.local>
2019-12-09 13:33:13 -0500

    services tab done

--- ccc/app/view/services/emergency-services.php
+++ ccc/app/view/services/emergency-services.php
@@ -6,7 +6,7 @@
         <div class="btn btn-static" style="background-color: #f26621;">CALL US: 540-231-6557</div>
     </div>
 </div>
-
+<br>
 <h3><i>Worried about Self-Harm or Suicidality?</i></h3>
 <p>If you are concerned about yourself or any Virginia Tech student being (or becoming) suicidal,
     please call the Cook Counseling Center at 540-231-6557 or one of the other resources listed below. The Cook
