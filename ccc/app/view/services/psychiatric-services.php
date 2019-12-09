5e97f9ac9a477fb356e878c2316776497334dd9d
Nicole Moore <nicolemoore@Johns-MacBook-Air.local>
2019-12-09 12:14:37 -0500

    psych page

--- ccc/app/view/services/psychiatric-services.php
+++ ccc/app/view/services/psychiatric-services.php
@@ -1,4 +1,21 @@
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
-<p>psychiatric services</p>
+<h1>Psychiatric Services</h1>
+
+<p>Psychiatric services through Cook Counseling Center are available to any Virginia Tech student who has paid
+    the student health fee. The process of psychiatric treatment begins by
+    <a href="<?=BASE_URL?>services/schedule-appointment">scheduling an appointment</a>
+    with a counselor.The counselor will evaluate your symptoms by performing a background screening and assessing
+    the results. It is up to your counselor to recommend psychiatric services if they are needed, however, they may
+    also recommend individual therapy, group therapy, or participation in one of Cook Counseling Center’s workshops
+    (such as relaxation training).
+</p>
+
+More information can be requested from Cook Counseling Center at <b>540-231-6557</b>.
+<br>
+<br>
+<h3>Services</h3>
+<ul>
+    <li>Psychiatric evaluations and treatment</li>
+    <li>Psychiatric medications</li>
+    <li>Individual and group therapy in collaboration with counseling staff</li>
+    <li>Treatment for ADHD (stimulant medication requires psychoeducational testing)</li>
+</ul>
