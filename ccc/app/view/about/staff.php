134c88946d45035ec25a157cbfe9d5f3b9636c8b
Hithesh Peddamekala <hitp98@vt.edu>
2019-12-09 22:36:03 -0500

    initial staff pg

--- ccc/app/view/about/staff.php
+++ ccc/app/view/about/staff.php
@@ -1,4 +1,80 @@
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
-<p>our staff</p>
+<h1>Staff</h1>
+<div class="card flex-row flex-wrap" style="margin-bottom: 3px;">
+	<div class="card-header border-0">
+		<img src="https://www.ucc.vt.edu/content/ucc_vt_edu/en/about/staff/ellie_sturgis/_jcr_content/bio-image.transform/s-medium/image.jpg"
+		 style="width: 70px">
+	</div>
+	<div class="card-block px-2" style="padding-top: 10px; margin-left: 5px">
+		<h4 class="card-title">Ellie Sturgis</h4>
+		<p class="card-text">Interim Director
+			<br>
+			<a href="mailto:esturgis@vt.edu">esturgis@vt.edu</a>
+			<br>
+			<a href="tel:540-231-6557">540-231-6557</a>
+		</p>
+	</div>
+</div>
+
+<div class="card flex-row flex-wrap" style="margin-bottom: 3px;">
+	<div class="card-header border-0">
+		<img src="https://www.ucc.vt.edu/content/ucc_vt_edu/en/about/staff/james_reinhard/_jcr_content/bio-image.transform/s-medium/image.jpg"
+		 style="width: 70px">
+	</div>
+	<div class="card-block px-2" style="padding-top: 10px; margin-left: 5px">
+		<h4 class="card-title">James Reinhard</h4>
+		<p class="card-text">Associate Director for Psychiatry
+			<br>
+			<a href="mailto:jreinh@vt.edu">jreinh@vt.edu</a>
+			<br>
+			<a href="tel:540-231-6557">540-231-6557</a>
+		</p>
+	</div>
+</div>
+
+<div class="card flex-row flex-wrap" style="margin-bottom: 3px;">
+	<div class="card-header border-0">
+		<img src="https://www.ucc.vt.edu/content/ucc_vt_edu/en/about/staff/charlotte_amenkhienan/_jcr_content/bio-image.transform/s-medium/image.jpg"
+		 style="width: 70px">
+	</div>
+	<div class="card-block px-2" style="padding-top: 10px; margin-left: 5px">
+		<h4 class="card-title">Charlotte Amenkhienan</h4>
+		<p class="card-text">Assistant Director for Outreach Services
+			<br>
+			<a href="mailto:camen@vt.edu">camen@vt.edu</a>
+			<br>
+			<a href="tel:540-231-6557">540-231-6557</a>
+		</p>
+	</div>
+</div>
+
+<div class="card flex-row flex-wrap" style="margin-bottom: 3px;">
+	<div class="card-header border-0">
+		<img src="https://www.ucc.vt.edu/content/ucc_vt_edu/en/about/staff/gary_bennett/jcr:content/bio-image.transform/s-medium/image.jpg"
+		 style="width: 70px">
+	</div>
+	<div class="card-block px-2" style="padding-top: 10px; margin-left: 5px">
+		<h4 class="card-title">Gary Bennett</h4>
+		<p class="card-text">Assistant Director for Sport Psychology
+			<br>
+			<a href="mailto:gabennet@vt.edu">gabennet@vt.edu</a>
+			<br>
+			<a href="tel:540-231-2556">540-231-2556</a>
+		</p>
+	</div>
+</div>
+
+<div class="card flex-row flex-wrap" style="margin-bottom: 3px;">
+	<div class="card-header border-0">
+		<img src="https://www.ucc.vt.edu/content/ucc_vt_edu/en/about/staff/cathye_betzel/jcr:content/bio-image.transform/s-medium/image.jpg"
+		 style="width: 70px">
+	</div>
+	<div class="card-block px-2" style="padding-top: 10px; margin-left: 5px">
+		<h4 class="card-title">Cathye Griffin Betzel</h4>
+		<p class="card-text">Assistant Director for Training
+			<br>
+			<a href="mailto:cbetzel@vt.edu">cbetzel@vt.edu</a>
+			<br>
+			<a href="tel:540-231-2556">540-231-6557</a>
+		</p>
+	</div>
+</div>
