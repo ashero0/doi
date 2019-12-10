945ff728731b66e94b6460944e41312f138a9778
Hithesh Peddamekala <hitp98@vt.edu>
2019-12-10 11:54:16 -0500

    added content to alcholism & add pg's

--- ccc/app/view/self-care/add.php
+++ ccc/app/view/self-care/add.php
@@ -1,4 +1,308 @@
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
-<p>attention deficit disorder</p>
+<h1>Attention Deficit Disorder Handbook</h1>
+<p>Attention deficit hyperactivity disorder (ADHD) is a brain-based disorder that affects all aspects of one’s life. ADHD is
+	characterized by inattention and/or hyperactivity, which is more frequent and severe than what is typically observed in
+	individuals at a comparable level of development. Those who exhibit extreme hyperactivity are commonly diagnosed with attention
+	deficit hyperactive disorder (ADHD-H). Those with primarily inattentive symptoms are diagnosed as ADHD-I. Individual with
+	both clusters of symptoms are diagnosed with ADHD-C. Unless properly treated, ADHD could interfere with one’s academic achievement,
+	self-esteem, and professional and personal relationships.</p>
+<p>A good understanding of the condition is essential in coping with an ADHD diagnosis. Educate yourself by talking to health
+	care professionals and to others who have ADHD, as well as by using other resources such as published books and pamphlets.</p>
+
+<div id="accordion">
+	<div class="card">
+		<div class="card-header" id="headingOne">
+			<h5 class="mb-0">
+				<button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+					Who has ADD?
+				</button>
+			</h5>
+		</div>
+		<div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+			<div class="card-body">
+				<p>People with Attention Deficit Hyperactivity Disorder (ADHD) tend to be of average or above-average intelligence. The
+					ADHD diagnosis includes individuals whose symptoms are primarily hyperactive (ADHD-H), inattentive (ADHD-I), or both
+					(ADHD-C). About three to five percent of the general population has ADHD, while approximately one to three percent of
+					college students are diagnosed with ADHD. The condition is diagnosed about three times more frequently in males than
+					in females.</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="card">
+		<div class="card-header" id="headingTwo">
+			<h5 class="mb-0">
+				<button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+					What Causes ADD in Adolescents and Adults?
+				</button>
+			</h5>
+		</div>
+		<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+			<div class="card-body">
+				<p>Experts believe that there is a substantial genetic component to ADHD and that the condition is caused by a neurochemical
+					disconnection between two parts of the frontal lobes inside the brain. This affects the central nervous system’s development
+					and causes impairment in the ability to concentrate.</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="card">
+		<div class="card-header" id="headingThree">
+			<h5 class="mb-0">
+				<button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+					Basic Characteristics of ADD
+				</button>
+			</h5>
+		</div>
+		<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+			<div class="card-body">
+				<p>There are three main characteristics of ADD:</p>
+				<p class="font-weight-bold">Inattention</p>
+				<ul>
+					<li>Difficulty organizing tasks</li>
+					<li>Difficulty staying on task (quick loss of interest) and maintaining effort</li>
+					<li>Difficulty with transitions or prioritizing tasks, following instructions, and completing school work</li>
+					<li>Problems with misplacing things needed for tasks</li>
+					<li>Becomes easily distracted by extraneous stimuli</li>
+					<li>Difficulty remembering daily activities</li>
+				</ul>
+
+				<p class="font-weight-bold">Hyperactivity</p>
+				<ul>
+					<li>Experiences minor motor restlessness, such as fidgeting of hands</li>
+					<li>Difficulty remaining seated and talks excessively</li>
+					<li>Difficulty regulating restlessness to situational demands</li>
+					<li>Difficulty channeling physical restlessness in productive directions</li>
+					<li>Difficulty listening to others</li>
+					<li>Becomes easily distracted while reading</li>
+				</ul>
+				<p class="font-weight-bold">Impulsiveness</p>
+				<ul>
+					<li>Speaks or acts without considering the consequence</li>
+					<li>Difficulty taking turns</li>
+					<li>Feelings of being out-of-control, which can result in obsessive compulsive behaviors</li>
+					<li>Has need for high stimulus activity</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="card">
+		<div class="card-header" id="headingFour">
+			<h5 class="mb-0">
+				<button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+					Legal Issues for College ADD Students
+				</button>
+			</h5>
+		</div>
+		<div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+			<div class="card-body">
+				<p>Under the Americans with Disabilities Act of 1990 and The Individuals with Disabilities Education Act of 1975, students
+					with ADHD are eligible for legal recourse if they feel they have experienced classroom discrimination. These acts protect
+					all students, regardless of their academic level.</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="card">
+		<div class="card-header" id="headingFive">
+			<h5 class="mb-0">
+				<button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+					Success Strategies for College ADD Students
+				</button>
+			</h5>
+		</div>
+		<div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+			<div class="card-body">
+				<p class="font-weight-bold">Successful Transition Strategies</p>
+				<ul>
+					<li>Carefully plan your first semester by choosing courses that maximize your strengths.</li>
+					<li>Use available academic support services such as peer tutors.
+						<b>Do not wait until you are failing to seek help.</b>
+					</li>
+					<li>Remember that the typical ADHD student requires three times as much study time as the average student.</li>
+					<li>As an ADHD student, plan on spending more than four years to successfully complete your bachelor's degree.</li>
+					<li>Avoid drinking alcohol. Alcohol can impair academic functioning in any student. However, studies have shown that alcohol
+						negatively affects ADHD students at twice the rate of the average student.</li>
+				</ul>
+
+				<p class="font-weight-bold">Class Scheduling Strategies</p>
+				<ul>
+					<li>Try to schedule classes at times when you feel you are most alert.</li>
+					<li>Do not take more than two quantitative classes in a day.</li>
+					<li>Do not take more than two back-to-back classes in a semester.</li>
+					<li>Avoid taking several classes with especially demanding reading or writing requirements during a single semester.</li>
+					<li>Do not take more than twelve hours per semester.</li>
+					<li>Be aware of drop/add dates. Consider dropping a class if a professor is not willing to accommodate you.</li>
+					<li>If possible, plan on taking especially difficult classes on a pass/fail basis</li>
+				</ul>
+
+				<p class="font-weight-bold">In-class Strategies</p>
+				<ul>
+					<li>Do not sit by the window.</li>
+					<li>Tape record class lectures when possible or use your professor’s notes.</li>
+					<li>Participate in class discussions as much as possible to enhance your concentration.</li>
+					<li>Try to schedule the timing of medication doses to maximize your class performance.</li>
+					<li>When unsure of the meaning of exam questions, ask the professor. Inform the professor of this possibility prior to your
+						exam.
+					</li>
+				</ul>
+
+				<p class="font-weight-bold">Effective Study Strategies</p>
+				<ul>
+					<li>Study in a distraction-free environment – you should not study in your room.</li>
+					<li>Study in one-hour blocks. Do not expect to be able to study for extended periods of time.</li>
+					<li>Study with others to reinforce your understanding of the material.</li>
+					<li>Know your learning style – auditory, visual, or kinesthetic – to maximize learning; take this information into consideration
+						when working with a tutor.</li>
+					<li>When you study alone, dictate key points into a tape recorder to enhance learning.</li>
+					<li>If you are an auditory learner, read aloud to yourself while studying.</li>
+					<li>Always "over-learn" the material. Students with memory or attention problems need to practice or rehearse more than
+						the average student.</li>
+					<li>Use active learning techniques, such as SQ3R, that allow you to do the following:
+					</li>
+					<ul>
+						<li>Skim a chapter to determine both content and important points.</li>
+						<li>Change chapter headings into questions.</li>
+						<li>Read the text to find answers to your questions.</li>
+						<li>Recite answers to yourself.</li>
+						<li>Review the major points. Read each heading and write down the main points you can recall.</li>
+					</ul>
+				</ul>
+
+				<p class="font-weight-bold">Organization and Time Management Strategies</p>
+				<p>Managing time is a major problem for college ADHD students because of the considerable amount of unstructured time they
+					have in college. The college ADHD student will need to devote more time to studying than the average college student.</p>
+				<ul>
+					<li>Learn to use a daily planner.</li>
+					<ul>
+						<li>Put all due dates and exams in the planner.</li>
+						<li>Break reading assignments into little bits and schedule times to do the reading.</li>
+						<li>Include all your social, work, and recreational activities in your planner.</li>
+						<li>Always have your planner with you.</li>
+					</ul>
+					<li>Allocate 10-15 minutes in the morning, or before you go to bed at night, to plan your day.</li>
+					<li>Make a "To Do" list and keep it in your planner.</li>
+					<li>Make constructive use of your time between classes, e.g., catching up on assignments, doing assigned reading, etc.</li>
+				</ul>
+
+			</div>
+		</div>
+	</div>
+
+	<div class="card">
+		<div class="card-header" id="headingSix">
+			<h5 class="mb-0">
+				<button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+					Stratgies for Coping with ADD at Work
+				</button>
+			</h5>
+		</div>
+		<div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+			<div class="card-body">
+				<p class="font-weight-bold">Coping with Hyperactivity</p>
+				<ul>
+					<li>Consider a job that enables you to move around more regularly. Avoid desk jobs that require long meetings.</li>
+					<li>If employed in a desk job, take frequent breaks – every 20-30 minutes or so.</li>
+					<li>Engage in some regular physical activity after you get off work.</li>
+					<li>If you have to attend a long meeting, bring something you can do inconspicuously with your hands during the meeting.</li>
+					<li>When being considered for a position that would not allow for much physical movement, consider your ability to successfully
+						cope before accepting.</li>
+				</ul>
+
+				<p class="font-weight-bold">Coping with Distraction</p>
+				<ul>
+					<li>Try to schedule work periods during which you will not be interrupted. Inform your supervisor and subordinates of the
+						importance of this to your job performance.</li>
+					<li>Schedule intensive work in short, time-limited chunks.</li>
+					<li>If you are distracted by a rapid flow of ideas tangentially related to a current project or other projects, write the
+						intruding thoughts down or record them on a voice recorder in a "brainstorming log."</li>
+					<li>When making meeting presentations, use an outline of what you must say and stick to it.</li>
+					<li>If you become distracted by your own ideas while listening to others speak at meetings, write down your ideas as you
+						listen.
+					</li>
+					<li>Keep your workspace orderly. Do not, however, use desk organization as a distraction from tasks that must be done.</li>
+					<li>If you become distracted by returning phone calls, opening mail etc., train yourself to avoid these diversions and keep
+						on task.</li>
+				</ul>
+
+				<p class="font-weight-bold">Staying Organized</p>
+				<ul>
+					<li>If you have a problem with staying organized and following-through, work with someone for whom these are strengths.</li>
+					<li>Jobs that involve many short-term projects are more compatible with adult ADHD than longer-term projects.</li>
+					<li>If your job involves receiving tasks from several different people, have your direct supervisor "filter" your work assignments
+						before they get to you.</li>
+				</ul>
+
+				<p class="font-weight-bold">Managing Time on the Job</p>
+				<ul>
+					<li>Pro-actively plan activities instead of reacting to events, impulses, or moods.</li>
+					<li>Prioritize activities on your "To Do" list.</li>
+					<li>Avoid over-scheduling your day.</li>
+					<li>Break large projects into small manageable parts. Set deadlines for each part.</li>
+					<li>Have your planner with you at all times. If unable to start a new task right away, put it on your "To Do" list.</li>
+					<li>Do not say "yes" when you mean "no." When asked to do something, do not respond impulsively; instead, learn to check
+						on previous commitments before responding.</li>
+					<li>Learn to say "no" to last-minute impulses, unless they are genuine emergencies. ADD adults are chronically late due
+						to impulses to handle "brief" last-minute tasks on their way out the door.</li>
+					<li>Avoid getting caught in hallway conversations when on your way to important meetings.</li>
+					<li>Leave early for meetings and take some work with you, should there be any "down-time" during the meeting.</li>
+					<li>Phone calls may take more time than you expect, so learn to end calls a few minutes early. This leaves more time for
+						planned tasks.</li>
+
+				</ul>
+
+				<p class="font-weight-bold">Memory</p>
+				<ul>
+					<li>Record the audio for important meetings and seminars. Be sure to set track markers to help you locate important aspects
+						of the session.</li>
+					<li>Take notes at meetings, even though you may also be recording them.</li>
+					<li>Take notes whenever you meet with your supervisor or a co-worker; never rely on memory alone.</li>
+					<li>To double-check accuracy, always send copies of your interpretation of agreements or commitments to the individuals
+						involved.</li>
+				</ul>
+
+			</div>
+		</div>
+	</div>
+
+	<div class="card">
+		<div class="card-header" id="headingSeven">
+			<h5 class="mb-0">
+				<button class="btn btn-link accordion-button" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+				Resources Available to ADD Students at Virginia Tech
+				</button>
+			</h5>
+		</div>
+		<div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
+			<div class="card-body">
+			<p><b>Services for Students with Disabilities</b><br>
+			310 Lavery Hall 0185  430 Old Turner Street<br>
+			Phone: 540-231-0858<br>
+			Fax: 540-231-0848<br>
+			TTY: 540-231-0853</p>
+			<p><b>Tutorial Services</b><p>
+			<p><b>Student Success Center</b><br>
+			110 Femoyer Hall  280 Stanger Street<br>
+			Phone: 540-231-4133<p>
+			<p><b>Thomas E. Cook Counseling Center</b><br>
+			240 McComas Hall<br>
+			Phone: 540-231-6557</p>
+			<p>Services Offered</p>
+			<ul>
+				<li>Individual counseling</li>
+				<li>Group counseling</li>
+				<li>Referral to Student Health Services when needed</li>
+				<li>Study skills counseling</li>
+			</ul>
+			<p><b>Newman Library</b><br>
+			Special Services Lab<br>
+			First Floor<br>
+			Phone: 540-231-4270<br>
+			Email: <a href="mailto:Email-assit@vt.edu">Email-assit@vt.edu</a></p>
+			</div>
+			
+
+		</div>
+	</div>
+
+</div>
--- ccc/app/view/self-care/alcoholism.php
+++ ccc/app/view/self-care/alcoholism.php
@@ -1,4 +1,11 @@
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
-<p>self care </p>
+<h1>Do You Have Trouble with Drinking?</h1>
+<p> Alcohol abuse is one of the leading problems suffered by college students. Alcohol can have significant effects on feelings,
+	perceptions and physiology. This kind of abuse can affect your personality, academic performance, relationships and your
+	health.</p>
+<p> If you feel that you may have a problem controlling your alcohol intake or have developed a dependency on alcohol, Cook Counseling
+	Center urges you to seek help. We provide brief, individual counseling to students whose alcohol use may be impairing academic
+	performance, as well as referral to more specialized treatment facilities for more serious substance abuse problems.</p>
+<p>Another option for students is attending
+	<a href="http://www.aa.org/?Media=PlayFlash"> Alcoholics Anonymous</a>. There are
+	<a href="https://aavirginia.org/meetings/">daily meetings</a> throughout the Blacksburg area. Alcoholics Anonymous is a free fellowship where groups of people share
+	their experiences while striving to help one another recover from alcoholism.</p>
