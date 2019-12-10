23f09ab6b39b7bbd90f1dc19e04ed9c3ac3d6cf7
Barbara O'Connor <barbaraoconnor@Barbaras-MacBook-Pro.local>
2019-12-10 01:18:00 -0500

    working on home page design still

--- ccc/app/view/home.php
+++ ccc/app/view/home.php
@@ -1,7 +1,53 @@
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100"
+                src="https://vtnews.vt.edu/content/vtnews_vt_edu/en/articles/2016/02/020516-dsa-cookreaccred/jcr:content/article-image.transform/xl-medium/image.jpg"
+                alt="First slide">
+            <div class="carousel-caption d-none d-md-block" style="background-color: #861F40">
+                <h1 style="color: #FFFFFF;">Need an appointment?</h1>
+                <a href="<?=BASE_URL?>services/schedule-appointment" style="color: #FFFFFF;">
+                    <h6>Schedule one now<svg aria-hidden="true" focusable="false" data-prefix="fal"
+                            data-icon="arrow-right" style="color: #FFFFFF;" height=12 width=13 role="img"
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
+                            <path fill="currentColor"
+                                d="M216.464 36.465l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L387.887 239H12c-6.627 0-12 5.373-12 12v10c0 6.627 5.373 12 12 12h375.887L209.393 451.494c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l211.051-211.05c4.686-4.686 4.686-12.284 0-16.971L233.434 36.465c-4.686-4.687-12.284-4.687-16.97 0z">
+                            </path>
+                        </svg></h6>
+                </a>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100"
+                src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/70640555_1487175351424905_8290028540527640576_o.jpg?_nc_cat=101&_nc_ohc=BTJpuJnrQdAAQkobnWiti4_zMcUQRXAZr69kBBT6UBy5nw9fcgG91zeDA&_nc_ht=scontent-iad3-1.xx&oh=7648b4390ee7109edf4f9744c5f5dff0&oe=5E8514FA"
+                alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100"
+                src="https://www.ucc.vt.edu/content/ucc_vt_edu/en/index/_jcr_content/cta/adaptiveimage.img.jpg/1568739868914.jpg"
+                alt="Third slide">
+        </div>
+
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
 <p>this is the homepage code</p>
 <a href="<?=BASE_URL?>/about/contact-us">
-	<p>contact us</p>
+    <p>contact us</p>
 </a>
 <a href="<?=BASE_URL?>/about/hours-and-location">
-	<p>hours & location</p>
+    <p>hours & location</p>
 </a>
