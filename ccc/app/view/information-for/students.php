14076d9d7638fbc3fd069a447efe278e4a1e2503
Barbara O'Connor <barbaraoconnor@Barbaras-MacBook-Pro.local>
2019-12-11 15:33:25 -0500

    google maps iframe and some of the information for pages

---
+++ ccc/app/view/information-for/students.php
@@ -0,0 +1,49 @@
+<h1>Information for Students</h1>
+
+<p>Are you feeling stressed or overwhelmed? These are normal feelings for students going through the university experience. If you would like assistance managing issues in your life, we are here to help. If you have never been seen at the counseling center, you may call 540-231-6557 to schedule an initial appointment.</p>
+
+<p>We offer a variety of services including:
+<ul>
+    <li><a href="<?=BASE_URL?>services/group-counseling">Group counseling</a></li>
+    <li><a href="<?=BASE_URL?>services/individual-counseling">Individual counseling</a></li>
+    <li><a href="<?=BASE_URL?>services/psychiatric-services">Psychiatric services</a></li>
+</ul>
+<p>
+
+<p>If you aren't sure that counseling is the right thing for you right now, but still want helping coping, we offer self care and support techniques for many situations:
+<ul>
+    <li><a href="<?=BASE_URL?>self-care/trauma">Trauma</a></li>
+    <li><a href="<?=BASE_URL?>self-care/sexual-assault">Survivors of Sexual Assault</a></li>
+    <li><a href="<?=BASE_URL?>self-care/eating-disorders">Eating Disorders</a></li>
+    <li><a href="<?=BASE_URL?>self-care/alcoholism">Drinking</a></li>
+</ul>
+We also offer an online screening module that can let you know a best course of action to help you.</p>
+
+<h3>Worried about Self-Harm or Suicidality?</h3>
+
+<p>If you are concerned about yourself or any Virginia Tech student being (or becoming) suicidal, please call the Cook Counseling Center at 540-231-6557 or one of the other resources listed below. The Cook Counseling Center number can be used during regular office hours or after-hours to speak with a counselor.</p>
+
+<h3>Local Crisis / Support Resources:</h3>
+
+<p><a href="http://www.nrvcs.org/access-services/">ACCESS</a> – NRVCS (New River Valley Community Services) Emergency Services provides <b>emergency evaluation and crisis intervention</b> to the New River Valley 24 hours per day, 7 days per week. A friend or family member can call for you if you feel that you are unable to do so yourself. Call 540-961-8400.</p>
+
+<p><a href="http://www.nrvcs.org/raft/">RAFT</a> - The Raft Crisis Hotline is a program of NRVCS that offers <b>free paraprofessional phone counseling services</b> to the community.Volunteers provide suicide and crisis intervention, empathy and support, mental health and substance abuse information and referrals to the local communities including Virginia Tech and Radford University. Raft operates 4 p.m. – 8 a.m. Monday through Friday and 24 hours on the weekends. NRVCS Case Managers answer the phones during business hours and New Horizons, a residential crisis stabilization program, provides callers with services when volunteers are unavailable. Call 540-961-8400</p>
+
+<p><a href="https://www.carilionclinic.org/connect">CONNECT</a> – A 24-hour <b>referral and emergency evaluation service</b> of Carillion Health Care. Call 1-800-284-8898 or 540-981-8181.</p>
+
+<p><a href="https://lewisgale.com/service/mental-health-programs-and-services">ARC</a> – A <b>mental health admittance and referral service</b> of Montgomery Regional Hospital and Lewis Gale Hospital. Call 804-327-5191 or 877-886-7026.</p>
+
+<p><a href="https://www.police.vt.edu/index.html">Police</a> – 911</p>
+
+<h3>State / National Crisis & Support Resources:</h3>
+
+<p><a href="https://mhav.org/support/warm-line/">Mental Health America of Virginia Warm Line</a> - Warm Line is a peer-run service for residents of Virginia.The peers at MHAV are not licensed professionals, medical providers, or legal advisers. MHAV’s Warm Line can provide support for those who need someone to talk to, however crisis situations should be directed to another of the listed resources.</p>
+
+<p><a href="http://www.suicidepreventionlifeline.org/">National Suicide Prevention Lifeline</a> - Call 1-800-273-8255 (1-800-TALK)
+Available 24 hours a day</p>
+
+<p><a href="http://www.ulifeline.org/vt/get_help_now">ULifeline</a> - Text "START" to 741-741 or call 1-800-273-TALK (8255)</p>
+
+<p><a href="https://www.thetrevorproject.org/get-help-now/#sm.0000jm63h2ne2f52yca2dwru4zvql">The Trevor Lifeline</a> - (Suicide Prevention for LGBTQ Youth) 866-4-U-TREVOR (1-866-488-7386)</p>
+
+<p><a href="https://hotline.rainn.org/online/">National Sexual Assault Hotline</a> - 24-hour online hotline 1-800-656-HOPE (1-800-656-4673)</p>
