3d3ac2f6d62c1a44e7a94ff0da31e9c346ce32f9
Hithesh Peddamekala <hitp98@vt.edu>
2019-12-11 11:06:19 -0500

    finished self-care dropdown pg's

--- ccc/app/view/self-care/eating-disorders.php
+++ ccc/app/view/self-care/eating-disorders.php
@@ -1,4 +1,23 @@
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
-<p>eating disorders</p>
+<h1>Eating Disorders</h1>
+<p>The most common eating disorders on college campuses are anorexia nervosa and bulimia. Both of these disorders occur more
+	frequently in young women, bulimia being the more common. It is estimated that one in 20 college-age women have an eating
+	disorder. Both disorders have serious medical consequences.</p>
+<p>People who suffer from anorexia nervosa deliberately attempt to lose weight through self-starvation. Even though they may
+	be extremely underweight, they see themselves as “fat” or are disgusted with their body image, deny any problem with their
+	eating habits and will resist any efforts made to get them to eat or return to a more healthy weight.</p>
+<p>People who suffer from bulimia engage in frequent, often daily, binge eating where they eat very large amounts of food, most
+	likely in secret. They will then try to counteract the binge by purging, which may involve self-induced vomiting, use of
+	laxatives or prolonged fasting and exercise.</p>
+<p>While many people with anorexia nervosa or bulimia deny having a problem and are reluctant to seek help, the disorder often
+	comes to the attention of friends and family. A direct, caring and nonjudgmental expression of concern about their health
+	by friends and family is often a vital first step in enabling people with eating disorders to seek appropriate treatment.</p>
+<p>Cook Counseling Center and Schiffert Health Center offer individual psychotherapy, medical, and
+	<a href="https://healthcenter.vt.edu/ourservices/nutrition_services.html"
+	 target="_blank">nutritional counseling</a> for anyone suffering from these disorders. With proper help, people with eating disorders can
+	learn to stabilize their eating patterns, maintain a health weight and resume a healthy lifestyle.</p>
+<p>Students who wish more intensive follow-up can be referred to the
+	<a href="https://healthcenter.vt.edu/ourservices/nutrition_services/healthy_paths.html"
+	 target="_blank">HEART (Healthy Eating and Assessment Team)</a>. This is a multidisciplinary team comprised of a physician, nurse, psychologist,
+	nutritionist, and exercise specialist. These individuals will evaluate the student and recommend the appropriate level of
+	care (e.g., inpatient treatment, intensive outpatient treatment, or outpatient care). Virginia Tech does not have inpatient
+	or intensive outpatient services, so a referral would be made in these cases.</p>
--- ccc/app/view/self-care/trauma.php
+++ ccc/app/view/self-care/trauma.php
@@ -1,4 +1,12 @@
-<a href="<?=BASE_URL?>">
-	<p>go back</p>
-</a>
-<p>trauma</p>
+<h1>Trauma</h1>
+<p>Traumatic events can have immediate, unexpected, and long-lasting effects on a person. Cook Counseling Center recognizes this and has created the following documents for the university community about dealing with trauma. If you would like to speak to a counselor about any issue that you are currently dealing with, contact us.</p>
+<ul>
+	<li><a href="https://www.ucc.vt.edu/content/dam/ucc_vt_edu/assets/docs/dealing_with_aftermath_tragedy_in_classroom.pdf" target="_blank">Dealing with the Aftermath of Tragedy in the Classroom (PDF | 25KB)</a></li>
+	<li><a href="https://www.ucc.vt.edu/content/dam/ucc_vt_edu/assets/docs/suggestions_for_faculty_staff_when_dealing_with_tragedy.pdf" target="_blank">Suggestions for Faculty and Staff When Dealing with a Tragedy (PDF | 13KB)</a></li>
+	<li><a href="https://ucc.vt.edu/about/appointments/faculty_information.html" target="_blank">Faculty and Staff Information on Identifying and Referring a Distressed Student</a></li>
+</ul>
+<h3>External Resources</h3>
+<ul>
+	<li>United States Department of Veteran Affairs: <a href="https://www.ptsd.va.gov/gethelp/coping_stress_reactions.asp" target="_blank">Coping with Traumatic Stress Reactions</a></li>
+	<li>Substance Abuse and Mental Health Services Administration: <a href="https://www.samhsa.gov/find-help/disaster-distress-helpline/coping-tips" target="_blank">Coping with Disasters and Traumatic Events</a></li>
+	<li>National Institute of Mental Health: <a href="http://www.nimh.nih.gov/health/publications/post-traumatic-stress-disorder-ptsd/index.shtml" target="_blank">Post-Traumatic Stress Disorder (PTSD) Fact Sheet</a></li>
