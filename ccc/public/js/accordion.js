b4a909dd5d94ba3cfbf27d04a7e928850204093a
Heather Robinson <hnr2800@vt.edu>
2019-12-12 14:16:06 -0500

    add comments to js files

--- ccc/public/js/accordion.js
+++ ccc/public/js/accordion.js
@@ -1,3 +1,5 @@
+// For the accordions within the page,
+// rotate the plus icon when the card is opened.
 $(document).ready(function(event) {
 	$('.card').on('show.bs.collapse', function(event) {
 		$(this).find('.accordion-button i').toggleClass('rotate');
